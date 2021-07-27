0617e5b216f9b3c4f5efa7099eb32528ed57fec9
jv_aredess <joaovitoraredes.jvv@gmail.com>
2021-07-27 17:53:45 -0300

    Primeiro commit

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,13 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa(
-    val nome: String,
-    val cpf: String
-)
+class Pessoa {
+    var nome: String = "João Vitor" //deixando claro que só pode string para evitar de utilizarem números e etc
+    var cpf: String = "123.456.789.10" //usando como string, por conta dos pontos"."
+}
 
+fun main() {
+    val joao =  Pessoa()
+
+    println(joao.nome)
+    println(joao.cpf)
+}
