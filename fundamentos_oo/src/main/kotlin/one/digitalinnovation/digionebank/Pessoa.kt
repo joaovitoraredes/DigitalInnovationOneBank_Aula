5de9d572a8524835f72576258410d58dfe5b1db9
jv_aredess <joaovitoraredes.jvv@gmail.com>
2021-07-27 19:03:49 -0300

    Updated to apply the private modifier to set function of cpf property

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "João Vitor" //deixando claro que só pode string para evitar de utilizarem números e etc
     var cpf: String = "123.456.789.10" //usando como string, por conta dos pontos"."
-}
+    private set
+    }
+
 
 fun main() {
     val joao =  Pessoa()
