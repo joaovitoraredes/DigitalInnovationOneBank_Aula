520304cb8309a31ab39c47457fdbdd3534ab48df
jv_aredess <joaovitoraredes.jvv@gmail.com>
2021-07-28 10:14:57 -0300

    created dataclass "banco", and file BancoTeste.kt

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,12 +4,14 @@ class Pessoa {
     var nome: String = "João Vitor" //deixando claro que só pode string para evitar de utilizarem números e etc
     var cpf: String = "123.456.789.10" //usando como string, por conta dos pontos"."
     private set
+
+    fun pessoaInfo() = "$nome e $cpf"
+
     }
 
 
 fun main() {
     val joao =  Pessoa()
 
-    println(joao.nome)
-    println(joao.cpf)
+    println(joao.pessoaInfo())
 }
